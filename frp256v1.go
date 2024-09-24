4c8430b336698668686aec68c9d3eaedcbe56b58
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2024-09-24 14:24:11 -0300

    PKCS8 Support

--- frp256v1.go
+++ frp256v1.go
@@ -1,26 +1,260 @@
 // Parameters for the FRP256v1 Elliptic curve
-package frp256v1
+package main
 
 import (
+	"crypto/ecdsa"
 	"crypto/elliptic"
+	"encoding/asn1"
+	"errors"
 	"math/big"
 	"sync"
 )
 
-var initonce sync.Once
-var frp256v1 *elliptic.CurveParams
+var (
+	oidAnssip256 = asn1.ObjectIdentifier{1, 2, 250, 1, 223, 101, 256, 1}
+)
 
-func initFRP256v1() {
-	frp256v1 = new(elliptic.CurveParams)
-	frp256v1.P, _ = new(big.Int).SetString("f1fd178c0b3ad58f10126de8ce42435b3961adbcabc8ca6de8fcf353d86e9c03", 16)
-	frp256v1.N, _ = new(big.Int).SetString("f1fd178c0b3ad58f10126de8ce42435b53dc67e140d2bf941ffdd459c6d655e1", 16)
-	frp256v1.B, _ = new(big.Int).SetString("ee353fca5428a9300d4aba754a44c00fdfec0c9ae4b1a1803075ed967b7bb73f", 16)
-	frp256v1.Gx, _ = new(big.Int).SetString("b6b3d4c356c139eb31183d4749d423958c27d2dcaf98b70164c97a2dd98f5cff", 16)
-	frp256v1.Gy, _ = new(big.Int).SetString("6142e0f7c8b204911f9271f0f3ecef8c2701c307e8e4c9e183115a1554062cfb", 16)
-	frp256v1.BitSize = 256
+var initonce sync.Once
+var p256 *elliptic.CurveParams
+
+func init() {
+	initP256()
 }
 
-func FRP256v1() elliptic.Curve {
-	initonce.Do(initFRP256v1)
-	return frp256v1
+func initP256() {
+	p256 = new(elliptic.CurveParams)
+	p256.P, _ = new(big.Int).SetString("f1fd178c0b3ad58f10126de8ce42435b3961adbcabc8ca6de8fcf353d86e9c03", 16)
+	p256.N, _ = new(big.Int).SetString("f1fd178c0b3ad58f10126de8ce42435b53dc67e140d2bf941ffdd459c6d655e1", 16)
+	p256.B, _ = new(big.Int).SetString("ee353fca5428a9300d4aba754a44c00fdfec0c9ae4b1a1803075ed967b7bb73f", 16)
+	p256.Gx, _ = new(big.Int).SetString("b6b3d4c356c139eb31183d4749d423958c27d2dcaf98b70164c97a2dd98f5cff", 16)
+	p256.Gy, _ = new(big.Int).SetString("6142e0f7c8b204911f9271f0f3ecef8c2701c307e8e4c9e183115a1554062cfb", 16)
+	p256.BitSize = 256
+	p256.Name = "frp256v1"
+}
+
+func P256() elliptic.Curve {
+	initonce.Do(initP256)
+	return p256
+}
+
+// Define pkAlgorithmIdentifier to avoid undefined identifier
+type pkAlgorithmIdentifier struct {
+	Algorithm  asn1.ObjectIdentifier
+	Parameters asn1.RawValue
+}
+
+type PublicKey struct {
+	X, Y  *big.Int
+	Curve elliptic.Curve
+}
+
+type PrivateKey struct {
+	PublicKey PublicKey
+	D         *big.Int
+}
+
+func (pk *PublicKey) MarshalPKCS8PublicKey(curve elliptic.Curve) ([]byte, error) {
+	// Marshal the public key coordinates
+	derBytes := elliptic.Marshal(curve, pk.X, pk.Y)
+
+	// Determine the OID based on the curve
+	var oid asn1.ObjectIdentifier
+	switch curve {
+	case P256():
+		oid = oidAnssip256
+	default:
+		return nil, errors.New("unsupported curve")
+	}
+
+	// Create a SubjectPublicKeyInfo structure
+	subjectPublicKeyInfo := struct {
+		Algorithm pkAlgorithmIdentifier
+		PublicKey asn1.BitString
+	}{
+		Algorithm: pkAlgorithmIdentifier{
+			Algorithm:  oid,
+			Parameters: asn1.RawValue{Tag: asn1.TagOID, Bytes: []byte(oid.String())},
+		},
+		PublicKey: asn1.BitString{Bytes: derBytes, BitLength: len(derBytes) * 8},
+	}
+
+	// Marshal the SubjectPublicKeyInfo structure
+	derBytes, err := asn1.Marshal(subjectPublicKeyInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	return derBytes, nil
+}
+
+func ParsePublicKey(der []byte) (*PublicKey, error) {
+	var publicKeyInfo struct {
+		Algorithm pkAlgorithmIdentifier
+		PublicKey asn1.BitString
+	}
+
+	_, err := asn1.Unmarshal(der, &publicKeyInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	var curve elliptic.Curve
+	switch {
+	case publicKeyInfo.Algorithm.Algorithm.Equal(oidAnssip256):
+		curve = P256()
+	default:
+		return nil, errors.New("unsupported curve OID")
+	}
+
+	// Check if the public key bytes are empty
+	if len(publicKeyInfo.PublicKey.Bytes) == 0 {
+		return nil, errors.New("public key bytes are empty")
+	}
+
+	// Unmarshal the public key coordinates
+	X, Y := elliptic.Unmarshal(curve, publicKeyInfo.PublicKey.Bytes)
+	if X == nil || Y == nil {
+		return nil, errors.New("failed to unmarshal public key")
+	}
+
+	// Return the parsed public key with the determined curve
+	return &PublicKey{X: X, Y: Y, Curve: curve}, nil
+}
+
+func (pk *PrivateKey) MarshalPKCS8PrivateKey(curve elliptic.Curve) ([]byte, error) {
+	if !curve.IsOnCurve(pk.PublicKey.X, pk.PublicKey.Y) {
+		return nil, errors.New("Public key is not on the curve")
+	}
+
+	// Convert the private key D to bytes
+	dBytes := pk.D.Bytes()
+
+	curveSize := (curve.Params().BitSize + 7) / 8
+	if len(dBytes) < curveSize {
+		padding := make([]byte, curveSize-len(dBytes))
+		dBytes = append(padding, dBytes...)
+	}
+
+	// Determine the OID based on the curve
+	var oid asn1.ObjectIdentifier
+	switch curve {
+	case P256():
+		oid = oidAnssip256
+	default:
+		return nil, errors.New("unsupported curve")
+	}
+
+	// Create a PrivateKeyInfo structure
+	privateKeyInfo := struct {
+		Version             int
+		PrivateKeyAlgorithm pkAlgorithmIdentifier
+		PublicKey           struct {
+			X *big.Int
+			Y *big.Int
+		}
+		PrivateKey []byte
+	}{
+		Version: 0,
+		PrivateKeyAlgorithm: pkAlgorithmIdentifier{
+			Algorithm:  oid,
+			Parameters: asn1.RawValue{Tag: asn1.TagOID, Bytes: []byte(oid.String())},
+		},
+		PublicKey: struct {
+			X *big.Int
+			Y *big.Int
+		}{
+			X: new(big.Int).SetBytes(pk.PublicKey.X.Bytes()),
+			Y: new(big.Int).SetBytes(pk.PublicKey.Y.Bytes()),
+		},
+		PrivateKey: dBytes,
+	}
+
+	// Marshal the PrivateKeyInfo structure
+	derBytes, err := asn1.Marshal(privateKeyInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	return derBytes, nil
+}
+
+func ParsePrivateKey(der []byte) (*PrivateKey, error) {
+	var privateKeyInfo struct {
+		Version             int
+		PrivateKeyAlgorithm pkAlgorithmIdentifier
+		PublicKey           struct {
+			X *big.Int
+			Y *big.Int
+		}
+		PrivateKey []byte
+	}
+	_, err := asn1.Unmarshal(der, &privateKeyInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	// Determine the curve based on the OID
+	var curve elliptic.Curve
+	switch {
+	case privateKeyInfo.PrivateKeyAlgorithm.Algorithm.Equal(oidAnssip256):
+		curve = P256()
+	default:
+		return nil, errors.New("unsupported curve OID")
+	}
+
+	X := privateKeyInfo.PublicKey.X
+	Y := privateKeyInfo.PublicKey.Y
+	D := new(big.Int).SetBytes(privateKeyInfo.PrivateKey)
+
+	if !curve.IsOnCurve(X, Y) {
+		return nil, errors.New("Public key is not on the curve")
+	}
+
+	// Create and return the private key with the determined curve
+	privateKey := &PrivateKey{
+		PublicKey: PublicKey{
+			X:     X,
+			Y:     Y,
+			Curve: curve,
+		},
+		D: D,
+	}
+
+	return privateKey, nil
+}
+
+func (pk *PublicKey) ToECDSA() *ecdsa.PublicKey {
+	return &ecdsa.PublicKey{
+		Curve: pk.Curve,
+		X:     pk.X,
+		Y:     pk.Y,
+	}
+}
+
+func (pk *PrivateKey) ToECDSAPrivateKey() *ecdsa.PrivateKey {
+	return &ecdsa.PrivateKey{
+		PublicKey: ecdsa.PublicKey{
+			Curve: pk.PublicKey.Curve,
+			X:     pk.PublicKey.X,
+			Y:     pk.PublicKey.Y,
+		},
+		D: pk.D,
+	}
+}
+
+func NewPrivateKey(privateKey *ecdsa.PrivateKey) *PrivateKey {
+	return &PrivateKey{
+		PublicKey: PublicKey{
+			Curve: privateKey.PublicKey.Curve,
+			X:     privateKey.PublicKey.X,
+			Y:     privateKey.PublicKey.Y,
+		},
+		D: privateKey.D,
+	}
+}
+
+func ECDH(privateKey *ecdsa.PrivateKey, publicKey *ecdsa.PublicKey) ([]byte, error) {
+	// Compute shared key
+	x, _ := privateKey.Curve.ScalarMult(publicKey.X, publicKey.Y, privateKey.D.Bytes())
+	return x.Bytes(), nil
 }
